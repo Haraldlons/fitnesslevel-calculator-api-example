296f8d4e9f1a95fe20060081a7441256ccf4e1c2
Harald Lønsethagen <haraldlons@gmail.com>
2018-12-07 13:45:48 +0100

    cleanup

--- public/components/main.ctrl.js
+++ public/components/main.ctrl.js
@@ -259,56 +259,5 @@
             ]
         };
 
-        // function testGetResponseAPI2() {
-        //     const proxyurl = "https://cors-anywhere.herokuapp.com/"; // https://tinyurl.com/y9p4bfl6
-        //     const url = 'https://www.worldfitnesslevel.org/service/api/response';
-        //     // const data = {
-        //     //     "system_id": "",
-        //     //     "user_id": "",
-        //     //     "lang": "",
-        //     //     "userId": "asdf",
-        //     //     "systemId": "test",
-        //     //     "responseId": "",             //must be used if you want to update an existing response, or else remove or set blank
-        //     //     "input": {
-        //     //         "education": 12,            //see /types
-        //     //         "ethnicity": 1,             //see /types
-        //     //         "gender": 1,                //see /types
-        //     //         "workoutFrequency": 2,      //see /types
-        //     //         "workoutTime": 2,           //see /types
-        //     //         "hardness": 3,              //see /types
-        //     //         "restingPulse": 45,         //beats pr minute
-        //     //         "maximumHeartRate": 189,    //beats pr minute
-        //     //         "weight": 83,               //in kilograms
-        //     //         "height": 187,              //in centimeters
-        //     //         "age": 37,
-        //     //         "latitude": 63.4305,        //optional
-        //     //         "longitude": 10.3951,       //optional
-        //     //         "city": "Trondheim",        //optional
-        //     //         "country": "Norway",        //optional
-        //     //         "group": ""                 //optional - only use group in agreement with API provider
-        //     //     }
-        //     // };
-
-        //     $http({
-        //         url: proxyurl + url,
-        //         method: 'GET',
-        //         system_id: "test",
-        //         user_id: "asdf",
-        //         lang: "en",
-        //         headers: {"Accept": "application/json"},
-        //     }).then(function (data) {
-        //         console.log(data);
-        //     }).catch(function (data, status, headers, config) {
-        //         //todo: report to user
-        //         console.log("Error while doing");
-        //         $log.error({
-        //             data: data,
-        //             status: status,
-        //             headers: headers,
-        //             config: config
-        //         });
-        //     });
-        // }
-        // testGetResponseAPI2();
     }]); // End Ctrl
 })();
